7c8049ee36416fbbb80867529c2dd93d99604674
gzglss <71701750+gzglss@users.noreply.github.com>
2022-03-29 20:41:06 +0800

    Update 最长回文子串.py

--- Data-structure-and-algorithm/最长回文子串.py
+++ Data-structure-and-algorithm/最长回文子串.py
@@ -1,3 +1,5 @@
+#第一种dp
+#dp[i][j]表示s[i:j+1]以s[j]结尾的最长回文子串长度
 def longestPalindrome(self, s: str) -> str:
     dp=[[0 for _ in range(len(s))] for _ in range(len(s))]
     for i in range(len(s)):
@@ -13,4 +15,29 @@ def longestPalindrome(self, s: str) -> str:
             if dp[i][j]>maxlen:
                 maxlen=dp[i][j]
                 mlstr=s[j-dp[i][j]+1:j+1]
+    return mlstr
+
+
+#第二种dp
+#dp[i][j]表示s[i:j+1]是否为回文子串True or False
+def longestPalindrome(self, s: str) -> str:
+    dp=[[False]*len(s) for _ in range(len(s))]
+    for i in range(len(s)):
+        dp[i][i]=True
+    
+    maxlen=1
+    mlstr=s[0]
+    for l in range(2,len(s)+1):
+        for i in range(len(s)):
+            j=l+i-1
+            if j>=len(s):
+                break
+            if s[j]==s[i]:
+                if j-i<3:
+                    dp[i][j]=True
+                else:
+                    dp[i][j]=dp[i+1][j-1]
+            if dp[i][j] and l>maxlen:
+                maxlen=l
+                mlstr=s[i:j+1]
     return mlstr
