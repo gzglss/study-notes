38e834d801b028aa7641982e1f0fb35d8093786d
https://github.com/gzglss <gzg888777666@163.com>
2022-07-15 09:44:14 +0800

    ''

--- Data-structure-and-algorithm/myTrie.py
+++ Data-structure-and-algorithm/myTrie.py
@@ -6,37 +6,39 @@
 
 import collections
 
+
 class TrieNode:
     def __init__(self):
         # 定义字典存储的数据类型为TrieNode
-        self.children=collections.defaultdict(TrieNode)
+        self.children = collections.defaultdict(TrieNode)
         # 标志符，用以表示当前字符串是否存在，一般可以通过将其置为False来间接删除该字符串
-        #也可以定义数值来统计字符串个数
-        self.is_word=False
+        # 也可以定义数值来统计字符串个数
+        self.is_word = False
+
 
 class MyTrie:
     def __init__(self):
-        self.root=TrieNode()
-        self.max_depth=0
+        self.root = TrieNode()
+        self.max_depth = 0
 
-    def insert(self,word):
-        cur=self.root
-        depth=0
+    def insert(self, word):
+        cur = self.root
+        depth = 0
         for c in word:
-            cur=cur.children[c]
-            depth+=1
-        cur.is_word=True
-        self.max_depth=max(depth,self.max_depth)
+            cur = cur.children[c]
+            depth += 1
+        cur.is_word = True
+        self.max_depth = max(depth, self.max_depth)
 
-    def seach(self,word):
-        cur=self.root
+    def seach(self, word):
+        cur = self.root
         for c in word:
-            cur=cur.children.get(c)
+            cur = cur.children.get(c)
             if not cur:
                 return False
         return cur.is_word
 
-    def customfunc(self,string_):
+    def customfunc(self, string_):
         '''
         自定义相关功能
         '''
---
+++ layer/reduce_mem.py
@@ -0,0 +1,48 @@
+"""
+对数据框格式的数据精度进行转换
+节约内存
+"""
+
+import numpy as np
+
+
+def reduce_mem_usage(df):
+    """ iterate through all the columns of a dataframe and modify the data type
+        to reduce memory usage.
+    """
+    start_mem = df.memory_usage().sum() / 1024 ** 2
+    # print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+    # print("column = ", len(df.columns))
+    for i, col in enumerate(df.columns):
+        # if i % 50 == 0:
+        #     print(i)
+        try:
+            col_type = df[col].dtype
+
+            if col_type != object:
+                c_min = df[col].min()
+                c_max = df[col].max()
+                if str(col_type)[:3] == 'int':
+                    if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                        df[col] = df[col].astype(np.int8)
+                    elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                        df[col] = df[col].astype(np.int16)
+                    elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                        df[col] = df[col].astype(np.int32)
+                    elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                        df[col] = df[col].astype(np.int32)
+                else:
+                    if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                        df[col] = df[col].astype(np.float32)
+                    elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                        df[col] = df[col].astype(np.float32)
+                    else:
+                        df[col] = df[col].astype(np.float32)
+        except:
+            continue
+
+    end_mem = df.memory_usage().sum() / 1024 ** 2
+    # print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+    # print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+
+    return df
